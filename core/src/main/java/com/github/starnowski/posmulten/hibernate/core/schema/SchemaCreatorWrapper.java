f5ed93cc12e0d522a2f872a20bf6e74313619561
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-01 19:26:12 +0100

    #1 - Added SchemaCreator wrapper [skip ci]

---
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/schema/SchemaCreatorWrapper.java
@@ -0,0 +1,21 @@
+package com.github.starnowski.posmulten.hibernate.core.schema;
+
+import org.hibernate.boot.Metadata;
+import org.hibernate.tool.schema.spi.ExecutionOptions;
+import org.hibernate.tool.schema.spi.SchemaCreator;
+import org.hibernate.tool.schema.spi.SourceDescriptor;
+import org.hibernate.tool.schema.spi.TargetDescriptor;
+
+public class SchemaCreatorWrapper implements SchemaCreator {
+
+    private final SchemaCreator wrappedSchemaCreator;
+
+    public SchemaCreatorWrapper(SchemaCreator wrappedSchemaCreator) {
+        this.wrappedSchemaCreator = wrappedSchemaCreator;
+    }
+
+    public void doCreation(Metadata metadata, ExecutionOptions executionOptions, SourceDescriptor sourceDescriptor, TargetDescriptor targetDescriptor) {
+        this.wrappedSchemaCreator.doCreation(metadata, executionOptions, sourceDescriptor, targetDescriptor);
+        //TODO
+    }
+}
