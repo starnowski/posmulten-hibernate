da5053608d577becf11a4c1572dfe8fa7a995807
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-02 23:30:53 +0100

    #1 - Added database scripts [skip ci]

---
+++ test-utils/src/main/resources/create-database-owner.sql
@@ -0,0 +1,3 @@
+CREATE USER "posmhib4-owner" WITH
+NOSUPERUSER
+  ENCRYPTED PASSWORD 'posmhib4-owner';
---
+++ test-utils/src/main/resources/create-database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE posmulten_hibernate WITH
+  OWNER = "posmhib4-owner"
+ENCODING = 'UTF8'
+CONNECTION LIMIT = -1;
+
+GRANT ALL PRIVILEGES ON DATABASE posmulten_hibernate to "posmhib4-owner" ;
