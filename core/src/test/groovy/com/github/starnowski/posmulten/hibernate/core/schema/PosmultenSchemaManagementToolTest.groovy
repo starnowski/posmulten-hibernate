67478b900bdc9e459b7085891574eef2477403ba
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-02 12:23:26 +0100

    #1 - Added tests for the PosmultenSchemaManagementTool type [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/schema/PosmultenSchemaManagementToolTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/schema/PosmultenSchemaManagementToolTest.groovy
@@ -1,6 +1,9 @@
 package com.github.starnowski.posmulten.hibernate.core.schema
 
+import org.hibernate.boot.registry.selector.spi.StrategySelector
+import org.hibernate.service.spi.ServiceRegistryImplementor
 import org.hibernate.tool.schema.internal.SchemaCreatorImpl
+import org.hibernate.tool.schema.spi.SchemaFilterProvider
 import spock.lang.Specification
 
 class PosmultenSchemaManagementToolTest extends Specification {
@@ -8,8 +11,13 @@ class PosmultenSchemaManagementToolTest extends Specification {
     def "should return schema creator wrapper" ()
     {
         given:
-            //TODO init schema tool with serviceRegistry
+            ServiceRegistryImplementor serviceRegistry = Mock(ServiceRegistryImplementor)
+            StrategySelector selector = Mock(StrategySelector)
+            SchemaFilterProvider filterProvider = Mock(SchemaFilterProvider)
+            serviceRegistry.getService(StrategySelector) >> selector
+            selector.resolveDefaultableStrategy(_, _, _) >> filterProvider
             def tested = new PosmultenSchemaManagementTool()
+            tested.injectServices(serviceRegistry)
 
         when:
             def result = tested.getSchemaCreator(null)
