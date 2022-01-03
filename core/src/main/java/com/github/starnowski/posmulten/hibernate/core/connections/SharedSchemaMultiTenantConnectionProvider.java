931c64d1a05b825bd40a0ec3a0597db242ff1ff3
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-03 09:37:02 +0100

    #1 - Added code refactoring [skip ci]

--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/SharedSchemaMultiTenantConnectionProvider.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/SharedSchemaMultiTenantConnectionProvider.java
@@ -1,11 +1,13 @@
 package com.github.starnowski.posmulten.hibernate.core.connections;
 
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.hibernate.service.spi.ServiceRegistryAwareService;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class SharedSchemaMultiTenantConnectionProvider implements MultiTenantConnectionProvider {
+public class SharedSchemaMultiTenantConnectionProvider implements MultiTenantConnectionProvider, ServiceRegistryAwareService {
     public Connection getAnyConnection() throws SQLException {
         return null;
     }
@@ -32,5 +34,9 @@ public class SharedSchemaMultiTenantConnectionProvider implements MultiTenantCon
 
     public <T> T unwrap(Class<T> aClass) {
         return null;
+    }
+
+    public void injectServices(ServiceRegistryImplementor serviceRegistryImplementor) {
+
     }
 }
