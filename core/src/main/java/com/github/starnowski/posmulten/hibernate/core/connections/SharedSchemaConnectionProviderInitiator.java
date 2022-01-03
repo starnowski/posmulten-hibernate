e008b570c0e3d4e5f28396926677c04f3eb7e196
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-01-03 21:47:39 +0100

    #1 - Fixed hibernate configuration [skip ci]

---
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/SharedSchemaConnectionProviderInitiator.java
@@ -0,0 +1,26 @@
+package com.github.starnowski.posmulten.hibernate.core.connections;
+
+import org.hibernate.cfg.Environment;
+import org.hibernate.engine.jdbc.connections.internal.ConnectionProviderInitiator;
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SharedSchemaConnectionProviderInitiator extends ConnectionProviderInitiator {
+
+
+    @Override
+    public Class<ConnectionProvider> getServiceInitiated() {
+        return ConnectionProvider.class;
+    }
+
+    @Override
+    public ConnectionProvider initiateService(Map configurationValues, ServiceRegistryImplementor registry) {
+        //TODO
+        Map copiedMap = new HashMap(configurationValues);
+        copiedMap.remove(Environment.MULTI_TENANT);
+        return super.initiateService(configurationValues, registry);
+    }
+}
--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/SharedSchemaMultiTenantConnectionProvider.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/SharedSchemaMultiTenantConnectionProvider.java
@@ -1,26 +1,36 @@
 package com.github.starnowski.posmulten.hibernate.core.connections;
 
+import org.hibernate.engine.jdbc.connections.spi.ConnectionProvider;
 import org.hibernate.engine.jdbc.connections.spi.MultiTenantConnectionProvider;
+import org.hibernate.engine.jndi.spi.JndiService;
 import org.hibernate.service.spi.ServiceRegistryAwareService;
 import org.hibernate.service.spi.ServiceRegistryImplementor;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class SharedSchemaMultiTenantConnectionProvider implements MultiTenantConnectionProvider, ServiceRegistryAwareService {
+
+    private ConnectionProvider connectionProvider;
+    private JndiService jndiService;
+    private DataSource dataSource;
+    private ServiceRegistryImplementor serviceRegistry;
+
     public Connection getAnyConnection() throws SQLException {
-        return null;
+        //TODO Set dummy tenant (for all)
+        return connectionProvider.getConnection();
     }
 
     public void releaseAnyConnection(Connection connection) throws SQLException {
 
     }
 
-    public Connection getConnection(String s) throws SQLException {
+    public Connection getConnection(String tenant) throws SQLException {
         return null;
     }
 
-    public void releaseConnection(String s, Connection connection) throws SQLException {
+    public void releaseConnection(String tenant, Connection connection) throws SQLException {
 
     }
 
@@ -36,7 +46,41 @@ public class SharedSchemaMultiTenantConnectionProvider implements MultiTenantCon
         return null;
     }
 
-    public void injectServices(ServiceRegistryImplementor serviceRegistryImplementor) {
-
+    public void injectServices(ServiceRegistryImplementor serviceRegistry) {
+        //ConnectionProvider
+        ConnectionProvider connectionProvider = serviceRegistry.getService(ConnectionProvider.class);
+        //TODO init default tenant (tenant for any)
+        if (connectionProvider != null) {
+            this.connectionProvider = connectionProvider;
+        }
+        this.serviceRegistry = serviceRegistry;
+//        final Object dataSourceConfigValue = serviceRegistry.getService(ConfigurationService.class)
+//                .getSettings()
+//                .get(AvailableSettings.DATASOURCE);
+//        if (dataSourceConfigValue == null || !(dataSourceConfigValue instanceof String)) {
+//            throw new HibernateException("Improper set up of DataSourceBasedMultiTenantConnectionProviderImpl");
+//        }
+//        final String jndiName = (String) dataSourceConfigValue;
+//
+//        jndiService = serviceRegistry.getService(JndiService.class);
+//        if (jndiService == null) {
+//            throw new HibernateException("Could not locate JndiService from DataSourceBasedMultiTenantConnectionProviderImpl");
+//        }
+//
+//        final Object namedObject = jndiService.locate(jndiName);
+//        if (namedObject == null) {
+//            throw new HibernateException("JNDI name [" + jndiName + "] could not be resolved");
+//        }
+////TODO init default tenant (tenant for any)
+//        if (namedObject instanceof DataSource) {
+//            this.dataSource = (DataSource) namedObject;
+//        } else if (namedObject instanceof Context) {
+//            //TODO init default tenant (tenant for any)
+//        } else {
+//            throw new HibernateException(
+//                    "Unknown object type [" + namedObject.getClass().getName() +
+//                            "] found in JNDI location [" + jndiName + "]"
+//            );
+//        }
     }
 }
