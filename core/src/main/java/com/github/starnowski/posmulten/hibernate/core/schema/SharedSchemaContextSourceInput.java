591bb67189d458dea597401ec2417f14e1df41f2
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-02-06 14:30:01 +0100

    #1 - Added implementation for the SharedSchemaContextSourceInput type [skip ci]

--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/schema/SharedSchemaContextSourceInput.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/schema/SharedSchemaContextSourceInput.java
@@ -1,5 +1,6 @@
 package com.github.starnowski.posmulten.hibernate.core.schema;
 
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition;
 import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext;
 import org.hibernate.tool.hbm2ddl.ImportSqlCommandExtractor;
 import org.hibernate.tool.schema.spi.ScriptSourceInput;
@@ -9,6 +10,7 @@ import java.util.List;
 public class SharedSchemaContextSourceInput implements ScriptSourceInput {
 
     private final ISharedSchemaContext context;
+    List<SQLDefinition> definitions;
 
     public SharedSchemaContextSourceInput(ISharedSchemaContext context) {
         this.context = context;
@@ -16,7 +18,7 @@ public class SharedSchemaContextSourceInput implements ScriptSourceInput {
 
     @Override
     public void prepare() {
-
+        this.definitions = context.getSqlDefinitions();
     }
 
     @Override
