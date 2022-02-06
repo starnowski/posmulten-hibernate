b6acfc3bdf90b473a5262f47a8b9b135dff5777c
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-02-06 14:29:10 +0100

    #1 - Added test for the SharedSchemaContextSourceInput type [skip ci]

---
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/schema/SharedSchemaContextSourceInputTest.groovy
@@ -0,0 +1,23 @@
+package com.github.starnowski.posmulten.hibernate.core.schema
+
+import com.github.starnowski.posmulten.postgresql.core.common.SQLDefinition
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext
+import spock.lang.Specification
+
+class SharedSchemaContextSourceInputTest extends Specification {
+
+    def "should prepare sql definitions" () {
+        given:
+            def sharedContext = Mock(ISharedSchemaContext)
+            def tested = new SharedSchemaContextSourceInput(sharedContext)
+            tested.definitions == null
+            List<SQLDefinition> definitions = Arrays.asList(Mock(SQLDefinition))
+
+        when:
+            tested.prepare()
+
+        then:
+            1 * sharedContext.getSqlDefinitions() >> definitions
+            tested.definitions == definitions
+    }
+}
