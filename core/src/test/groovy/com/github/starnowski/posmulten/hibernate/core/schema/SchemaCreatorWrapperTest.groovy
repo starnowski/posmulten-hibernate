40f815444629870d6b9a5bbe593c0c9a8f13fc47
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-02-08 09:41:02 +0100

    #1 - Added tests case for schema creation [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/schema/SchemaCreatorWrapperTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/schema/SchemaCreatorWrapperTest.groovy
@@ -1,7 +1,10 @@
 package com.github.starnowski.posmulten.hibernate.core.schema
 
+import com.github.starnowski.posmulten.hibernate.core.context.IDefaultSharedSchemaContextBuilderMetadataEnricher
 import com.github.starnowski.posmulten.hibernate.core.context.IDefaultSharedSchemaContextBuilderMetadataEnricherProvider
 import com.github.starnowski.posmulten.hibernate.core.context.IDefaultSharedSchemaContextBuilderProvider
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext
 import org.hibernate.boot.Metadata
 import org.hibernate.service.ServiceRegistry
 import org.hibernate.tool.schema.spi.ExecutionOptions
@@ -23,21 +26,27 @@ class SchemaCreatorWrapperTest extends Specification {
             TargetDescriptor targetDescriptor = Mock(TargetDescriptor)
             IDefaultSharedSchemaContextBuilderProvider defaultSharedSchemaContextBuilderProvider = Mock(IDefaultSharedSchemaContextBuilderProvider)
             IDefaultSharedSchemaContextBuilderMetadataEnricherProvider defaultSharedSchemaContextBuilderMetadataEnricherProvider = Mock(IDefaultSharedSchemaContextBuilderMetadataEnricherProvider)
-            SourceDescriptorFactory sourceDescriptorFactory = Mock(SourceDescriptorFactory)
+            IDefaultSharedSchemaContextBuilderMetadataEnricher enricher = Mock(IDefaultSharedSchemaContextBuilderMetadataEnricher)
+            SchemaCreatorStrategyContext schemaCreatorStrategyContext = Mock(SchemaCreatorStrategyContext)
+            DefaultSharedSchemaContextBuilder builder = Mock(DefaultSharedSchemaContextBuilder)
+            ISharedSchemaContext sharedSchemaContext = Mock(ISharedSchemaContext)
 
         when:
             tested.doCreation(metadata, executionOptions, sourceDescriptor, targetDescriptor)
 
         then:
             1 * mockedServiceRegistry.getService(IDefaultSharedSchemaContextBuilderProvider) >> defaultSharedSchemaContextBuilderProvider
-
-        then:
             1 * mockedServiceRegistry.getService(IDefaultSharedSchemaContextBuilderMetadataEnricherProvider) >> defaultSharedSchemaContextBuilderMetadataEnricherProvider
+            1 * mockedServiceRegistry.getService(SchemaCreatorStrategyContext) >> schemaCreatorStrategyContext
 
         then:
-            1 * mockedServiceRegistry.getService(SourceDescriptorFactory) >> sourceDescriptorFactory
-
-        then:
+            1 * defaultSharedSchemaContextBuilderProvider.get() >> builder
+            1 * enricher.enrich(builder, metadata) >> builder
+            1 * defaultSharedSchemaContextBuilderMetadataEnricherProvider.getEnrichers() >> [enricher]
             1 * mockedSchemaCreator.doCreation(metadata, executionOptions, sourceDescriptor, targetDescriptor)
+            1 * builder.build() >> sharedSchemaContext
+
+        then:
+            1 * schemaCreatorStrategyContext.doCreation(sharedSchemaContext, mockedSchemaCreator, metadata, executionOptions, sourceDescriptor, targetDescriptor)
     }
 }
