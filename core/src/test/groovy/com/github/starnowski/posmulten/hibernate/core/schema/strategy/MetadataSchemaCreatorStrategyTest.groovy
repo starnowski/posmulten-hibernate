70a5929749067d579afa330286795da195ebce60
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-02-08 08:25:03 +0100

    #1 - Added implementation for the MetadataSchemaCreatorStrategy [skip ci]

---
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/schema/strategy/MetadataSchemaCreatorStrategyTest.groovy
@@ -0,0 +1,36 @@
+package com.github.starnowski.posmulten.hibernate.core.schema.strategy
+
+import com.github.starnowski.posmulten.hibernate.core.schema.SharedSchemaContextSourceInput
+import com.github.starnowski.posmulten.postgresql.core.context.ISharedSchemaContext
+import org.hibernate.boot.Metadata
+import org.hibernate.tool.schema.spi.*
+import spock.lang.Specification
+
+import static org.hibernate.tool.schema.SourceType.METADATA_THEN_SCRIPT
+
+class MetadataSchemaCreatorStrategyTest extends Specification {
+
+    def"should pass source with script source and type METADATA_THEN_SCRIPT to schema creator"(){
+        given:
+            ISharedSchemaContext context = Mock(ISharedSchemaContext)
+            SchemaCreator schemaCreator = Mock(SchemaCreator)
+            Metadata metadata = Mock(Metadata)
+            ExecutionOptions executionOptions = Mock(ExecutionOptions)
+            SourceDescriptor sourceDescriptor = Mock(SourceDescriptor)
+            TargetDescriptor targetDescriptor = Mock(TargetDescriptor)
+            SourceDescriptor passedSourceDescriptor = null
+            def tested = new MetadataSchemaCreatorStrategy()
+
+        when:
+            tested.doCreation(context, schemaCreator, metadata, executionOptions, sourceDescriptor, targetDescriptor)
+
+        then:
+            1 * schemaCreator.doCreation(metadata, executionOptions, _, targetDescriptor) >> {
+                arguments ->
+                passedSourceDescriptor = (SourceDescriptor)arguments[2]
+            }
+            passedSourceDescriptor.getSourceType() == METADATA_THEN_SCRIPT
+            ScriptSourceInput si = passedSourceDescriptor.getScriptSourceInput()
+            si instanceof SharedSchemaContextSourceInput
+    }
+}
