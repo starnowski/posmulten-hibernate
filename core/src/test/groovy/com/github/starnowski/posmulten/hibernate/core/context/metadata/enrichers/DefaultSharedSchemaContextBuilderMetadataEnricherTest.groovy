380e2af6ef597000b484d185f7d5b0333807b310
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-02-20 12:33:36 +0100

    #1 - Added tests for the DefaultSharedSchemaContextBuilderMetadataEnricher component [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/enrichers/DefaultSharedSchemaContextBuilderMetadataEnricherTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/enrichers/DefaultSharedSchemaContextBuilderMetadataEnricherTest.groovy
@@ -1,6 +1,13 @@
 package com.github.starnowski.posmulten.hibernate.core.context.metadata.enrichers
 
+import com.github.starnowski.posmulten.hibernate.core.context.IDefaultSharedSchemaContextBuilderTableMetadataEnricher
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.enrichers.RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricher
+import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
+import org.hibernate.boot.Metadata
+import org.hibernate.boot.model.relational.Database
+import org.hibernate.boot.model.relational.Namespace
+import org.hibernate.mapping.Table
+import org.hibernate.service.spi.ServiceRegistryImplementor
 import spock.lang.Specification
 
 import java.util.stream.Collectors
@@ -45,7 +52,33 @@ class DefaultSharedSchemaContextBuilderMetadataEnricherTest extends Specificatio
 
         and: "enrichers should have correct type"
             results.stream().map({ it.getClass() }).collect(Collectors.toList()) == [RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricher.class]
-
     }
-    //TODO Test enriching builder with mocked enrichers (use setter)
+
+    def "should pass schema builder and metadata to table metadata enrichers"()
+    {
+        given:
+            def tested = new DefaultSharedSchemaContextBuilderMetadataEnricher()
+            def tableMetadataEnricher = Mock(IDefaultSharedSchemaContextBuilderTableMetadataEnricher)
+            def configurationMap = new HashMap()
+            def sr = Mock(ServiceRegistryImplementor)
+            tested.initiateService(configurationMap, sr)
+            tested.setEnrichers([tableMetadataEnricher])
+            DefaultSharedSchemaContextBuilder builder = Mock(DefaultSharedSchemaContextBuilder)
+            Metadata metadata = Mock(Metadata)
+            Namespace namespace = Mock(Namespace)
+            Database database = Mock(Database)
+            metadata.getDatabase() >> database
+            database.getNamespaces() >> [namespace]
+            Table table1 = Mock(Table)
+            Table table2 = Mock(Table)
+            namespace.getTables() >> [table1, table2]
+
+        when:
+            def result = tested.enrich(builder, metadata)
+
+        then:
+            result == builder
+            1 * tableMetadataEnricher.enrich(builder, metadata, table1) >> builder
+            1 * tableMetadataEnricher.enrich(builder, metadata, table2) >> builder
+    }
 }
