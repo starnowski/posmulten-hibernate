afe522072995bdafb819b48cc0ba7183d1b1c0ab
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-03-23 10:03:53 +0100

    #1 - Added tests case [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
@@ -1,7 +1,9 @@
 package com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.enrichers
 
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.PosmultenUtilContext
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.NameGenerator
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.PersistentClassResolver
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.TenantTableProperties
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.TenantTablePropertiesResolver
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import org.hibernate.boot.Metadata
@@ -9,6 +11,7 @@ import org.hibernate.mapping.PersistentClass
 import org.hibernate.mapping.Table
 import org.hibernate.service.spi.ServiceRegistryImplementor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest extends Specification {
 
@@ -64,5 +67,48 @@ class RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest extend
 
         and: "returned the same object of builder"
             result.is(builder)
+    }
+
+    @Unroll
+    def "should enrich builder with rls policy #policyName for table #tableName, tenant column #tenantColumnName and pass primary key columns #primaryKeysColumnAndTypeMap"(){
+        given:
+            def serviceRegistryImplementor = Mock(ServiceRegistryImplementor)
+            def posmultenUtilContext = Mock(PosmultenUtilContext)
+            def persistentClassResolver = Mock(PersistentClassResolver)
+            serviceRegistryImplementor.getService(PosmultenUtilContext) >> posmultenUtilContext
+            posmultenUtilContext.getPersistentClassResolver() >> persistentClassResolver
+            def builder = Mock(DefaultSharedSchemaContextBuilder)
+            def metadata = Mock(Metadata)
+            def table = Mock(Table)
+            def persistentClass = Mock(PersistentClass)
+            persistentClassResolver.resolve(metadata, table) >> persistentClass
+            def tenantTablePropertiesResolver = Mock(TenantTablePropertiesResolver)
+            posmultenUtilContext.getTenantTablePropertiesResolver() >> tenantTablePropertiesResolver
+            def tenantTableProperties = Mock(TenantTableProperties)
+            tenantTableProperties.getTable() >> tableName
+            tenantTableProperties.getPrimaryKeysColumnAndTypeMap() >> primaryKeysColumnAndTypeMap
+            tenantTableProperties.getTenantColumnName() >> tenantColumnName
+            tenantTablePropertiesResolver.resolve(persistentClass, table, metadata) >> tenantTableProperties
+            def nameGenerator = Mock(NameGenerator)
+            nameGenerator.generate("rls_policy_", table) >> policyName
+            posmultenUtilContext.getNameGenerator() >> nameGenerator
+
+            tested.init(null, serviceRegistryImplementor)
+
+        when:
+            def result = tested.enrich(builder, metadata, table)
+
+        then:
+            1 * builder.createRLSPolicyForTable(tableName, primaryKeysColumnAndTypeMap, tenantColumnName, policyName)
+            1 * builder.createTenantColumnForTable(tableName)
+
+        and: "returned the same object of builder"
+            result.is(builder)
+
+        where:
+            tableName           |   primaryKeysColumnAndTypeMap         |   tenantColumnName    |   policyName
+            "tab1"              |   [id: "varchar"]                     |   "ten_id"            |   "some_pol_rls"
+            "users"             |   [user_uuid: "UUID"]                 |   "tenant"            |   "policy_prefix"
+            "sys_users"         |   [id: "int", sys_uid: "varch"]       |   "customer"          |   "pol_rls"
     }
 }
