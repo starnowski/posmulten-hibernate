aac3f119e235b355c08001cecdc9006efe93ef85
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-03-23 19:52:53 +0100

    #1 - Added tests case [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
@@ -2,6 +2,7 @@ package com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.e
 
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.PosmultenUtilContext
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.CollectionResolver
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.NameGenerator
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.PersistentClassResolver
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import org.hibernate.boot.Metadata
@@ -10,6 +11,7 @@ import org.hibernate.mapping.PersistentClass
 import org.hibernate.mapping.Table
 import org.hibernate.service.spi.ServiceRegistryImplementor
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest extends Specification {
 
@@ -70,5 +72,45 @@ class JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest exten
 
         and: "returned the same object of builder"
             result.is(builder)
+    }
+
+    @Unroll
+    def "should enrich builder for table #tableName with policy #rlsPolicyName"(){
+        given:
+            def serviceRegistryImplementor = Mock(ServiceRegistryImplementor)
+            def posmultenUtilContext = Mock(PosmultenUtilContext)
+            def persistentClassResolver = Mock(PersistentClassResolver)
+            def collectionResolver = Mock(CollectionResolver)
+            serviceRegistryImplementor.getService(PosmultenUtilContext) >> posmultenUtilContext
+            posmultenUtilContext.getPersistentClassResolver() >> persistentClassResolver
+            posmultenUtilContext.getCollectionResolver() >> collectionResolver
+            def builder = Mock(DefaultSharedSchemaContextBuilder)
+            def metadata = Mock(Metadata)
+            def table = Mock(Table)
+            table.getName() >> tableName
+
+            def collection = Mock(Collection)
+            persistentClassResolver.resolve(metadata, table) >> null
+            collectionResolver.resolve(metadata, table) >> collection
+
+            def nameGenerator = Mock(NameGenerator)
+            posmultenUtilContext.getNameGenerator() >> nameGenerator
+            nameGenerator.generate("rls_policy_", table) >> rlsPolicyName
+
+            tested.init(null, serviceRegistryImplementor)
+
+        when:
+            def result = tested.enrich(builder, metadata, table)
+
+        then:
+            1 * builder.createRLSPolicyForTable(tableName, new HashMap<>(), null, rlsPolicyName)
+
+        and: "returned the same object of builder"
+            result.is(builder)
+
+        where:
+            tableName   |   rlsPolicyName
+            "tabxxx"    |   "policy_x1"
+            "roles"     |   "rls_policy"
     }
 }
