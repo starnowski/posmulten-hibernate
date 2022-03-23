6c22391e960507beed76add98c12669812e183f4
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-03-23 19:43:19 +0100

    #1 - Added tests case [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
@@ -5,6 +5,8 @@ import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.Co
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.PersistentClassResolver
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import org.hibernate.boot.Metadata
+import org.hibernate.mapping.Collection
+import org.hibernate.mapping.PersistentClass
 import org.hibernate.mapping.Table
 import org.hibernate.service.spi.ServiceRegistryImplementor
 import spock.lang.Specification
@@ -13,7 +15,7 @@ class JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest exten
 
     def tested = new JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricher()
 
-    def "should not enrich builder when Collection can not be resolve for table"(){
+    def "should not enrich builder when collection can not be resolve for table"(){
         given:
             def serviceRegistryImplementor = Mock(ServiceRegistryImplementor)
             def posmultenUtilContext = Mock(PosmultenUtilContext)
@@ -27,6 +29,36 @@ class JoinTablesDefaultSharedSchemaContextBuilderTableMetadataEnricherTest exten
             def table = Mock(Table)
             persistentClassResolver.resolve(metadata, table) >> null
             collectionResolver.resolve(metadata, table) >> null
+
+            tested.init(null, serviceRegistryImplementor)
+
+        when:
+            def result = tested.enrich(builder, metadata, table)
+
+        then:
+            0 * builder._
+
+        and: "returned the same object of builder"
+            result.is(builder)
+    }
+
+    def "should not enrich builder when persistentClass can be resolve for table"(){
+        given:
+            def serviceRegistryImplementor = Mock(ServiceRegistryImplementor)
+            def posmultenUtilContext = Mock(PosmultenUtilContext)
+            def persistentClassResolver = Mock(PersistentClassResolver)
+            def collectionResolver = Mock(CollectionResolver)
+            serviceRegistryImplementor.getService(PosmultenUtilContext) >> posmultenUtilContext
+            posmultenUtilContext.getPersistentClassResolver() >> persistentClassResolver
+            posmultenUtilContext.getCollectionResolver() >> collectionResolver
+            def builder = Mock(DefaultSharedSchemaContextBuilder)
+            def metadata = Mock(Metadata)
+            def table = Mock(Table)
+
+            def collection = Mock(Collection)
+            def persistentClass = Mock(PersistentClass)
+            persistentClassResolver.resolve(metadata, table) >> persistentClass
+            collectionResolver.resolve(metadata, table) >> collection
 
             tested.init(null, serviceRegistryImplementor)
 
