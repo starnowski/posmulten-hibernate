b5a5f58614801ce49ec897caf9693c33a8b02e51
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-03-23 01:39:41 +0100

    #1 - Added tests case [skip ci]

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
@@ -2,8 +2,10 @@ package com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.e
 
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.PosmultenUtilContext
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.PersistentClassResolver
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.TenantTablePropertiesResolver
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
 import org.hibernate.boot.Metadata
+import org.hibernate.mapping.PersistentClass
 import org.hibernate.mapping.Table
 import org.hibernate.service.spi.ServiceRegistryImplementor
 import spock.lang.Specification
@@ -23,6 +25,34 @@ class RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest extend
             def metadata = Mock(Metadata)
             def table = Mock(Table)
             persistentClassResolver.resolve(metadata, table) >> null
+
+            tested.init(null, serviceRegistryImplementor)
+
+        when:
+            def result = tested.enrich(builder, metadata, table)
+
+        then:
+            0 * builder._
+
+        and: "returned the same object of builder"
+            result.is(builder)
+    }
+
+    def "should not enrich builder when table properties can not be resolve for table"(){
+        given:
+            def serviceRegistryImplementor = Mock(ServiceRegistryImplementor)
+            def posmultenUtilContext = Mock(PosmultenUtilContext)
+            def persistentClassResolver = Mock(PersistentClassResolver)
+            serviceRegistryImplementor.getService(PosmultenUtilContext) >> posmultenUtilContext
+            posmultenUtilContext.getPersistentClassResolver() >> persistentClassResolver
+            def builder = Mock(DefaultSharedSchemaContextBuilder)
+            def metadata = Mock(Metadata)
+            def table = Mock(Table)
+            def persistentClass = Mock(PersistentClass)
+            persistentClassResolver.resolve(metadata, table) >> persistentClass
+            def tenantTablePropertiesResolver = Mock(TenantTablePropertiesResolver)
+            posmultenUtilContext.getTenantTablePropertiesResolver() >> tenantTablePropertiesResolver
+            tenantTablePropertiesResolver.resolve(persistentClass, table, metadata) >> null
 
             tested.init(null, serviceRegistryImplementor)
 
