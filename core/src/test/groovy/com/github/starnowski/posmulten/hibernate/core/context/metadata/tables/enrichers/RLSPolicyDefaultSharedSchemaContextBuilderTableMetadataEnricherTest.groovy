b097067f5af8308677f4d2a06ccee6e88444530e
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-04-19 00:33:16 +0200

    #4 - Code refactoring.

--- core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
+++ core/src/test/groovy/com/github/starnowski/posmulten/hibernate/core/context/metadata/tables/enrichers/RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest.groovy
@@ -1,10 +1,11 @@
 package com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.enrichers
 
 import com.github.starnowski.posmulten.hibernate.core.context.metadata.PosmultenUtilContext
-import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.*
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.PersistentClassResolver
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.RLSPolicyTableHelper
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.TenantTableProperties
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.tables.TenantTablePropertiesResolver
 import com.github.starnowski.posmulten.postgresql.core.context.DefaultSharedSchemaContextBuilder
-import com.github.starnowski.posmulten.postgresql.core.context.SharedSchemaContextRequest
-import com.github.starnowski.posmulten.postgresql.core.context.TableKey
 import org.hibernate.boot.Metadata
 import org.hibernate.mapping.PersistentClass
 import org.hibernate.mapping.Table
@@ -39,34 +40,6 @@ class RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest extend
             result.is(builder)
     }
 
-    def "should not enrich builder when table properties can not be resolve for table"(){
-        given:
-            def serviceRegistryImplementor = Mock(ServiceRegistryImplementor)
-            def posmultenUtilContext = Mock(PosmultenUtilContext)
-            def persistentClassResolver = Mock(PersistentClassResolver)
-            serviceRegistryImplementor.getService(PosmultenUtilContext) >> posmultenUtilContext
-            posmultenUtilContext.getPersistentClassResolver() >> persistentClassResolver
-            def builder = Mock(DefaultSharedSchemaContextBuilder)
-            def metadata = Mock(Metadata)
-            def table = Mock(Table)
-            def persistentClass = Mock(PersistentClass)
-            persistentClassResolver.resolve(metadata, table) >> persistentClass
-            def tenantTablePropertiesResolver = Mock(TenantTablePropertiesResolver)
-            posmultenUtilContext.getTenantTablePropertiesResolver() >> tenantTablePropertiesResolver
-            tenantTablePropertiesResolver.resolve(persistentClass, table, metadata) >> null
-
-            tested.init(null, serviceRegistryImplementor)
-
-        when:
-            def result = tested.enrich(builder, metadata, table)
-
-        then:
-            0 * builder._
-
-        and: "returned the same object of builder"
-            result.is(builder)
-    }
-
     @Unroll
     def "should enrich builder with rls policy #policyName for table #tableName, schema #schema, tenant column #tenantColumnName and pass primary key columns #primaryKeysColumnAndTypeMap"(){
         given:
@@ -83,33 +56,16 @@ class RLSPolicyDefaultSharedSchemaContextBuilderTableMetadataEnricherTest extend
             def tenantTablePropertiesResolver = Mock(TenantTablePropertiesResolver)
             posmultenUtilContext.getTenantTablePropertiesResolver() >> tenantTablePropertiesResolver
             def tenantTableProperties = Mock(TenantTableProperties)
-            tenantTableProperties.getTable() >> tableName
-            tenantTableProperties.getPrimaryKeysColumnAndTypeMap() >> primaryKeysColumnAndTypeMap
-            tenantTableProperties.getTenantColumnName() >> tenantColumnName
-            tenantTableProperties.getSchema() >> schema
             tenantTablePropertiesResolver.resolve(persistentClass, table, metadata) >> tenantTableProperties
-            def nameGenerator = Mock(NameGenerator)
-            nameGenerator.generate("rls_policy_", table) >> policyName
-            posmultenUtilContext.getNameGenerator() >> nameGenerator
-
-            def requestCopy = Mock(SharedSchemaContextRequest)
-            builder.getSharedSchemaContextRequestCopy() >> requestCopy
-            requestCopy.resolveTenantColumnByTableKey(new TableKey(tableName, schema)) >> tenantColumnName
-
-            def tableUtils = Mock(TableUtils)
-            posmultenUtilContext.getTableUtils() >> tableUtils
-            tableUtils.hasColumnWithName(table, tenantColumnName) >> false
-
+            def rlsPolicyHelper = Mock(RLSPolicyTableHelper)
+            posmultenUtilContext.getRlsPolicyTableHelper() >> rlsPolicyHelper
             tested.init(null, serviceRegistryImplementor)
 
         when:
             def result = tested.enrich(builder, metadata, table)
 
         then:
-            1 * builder.createRLSPolicyForTable(tableName, primaryKeysColumnAndTypeMap, tenantColumnName, policyName)
-
-        and: "create tenant column when column does not yet exist for table"
-            1 * builder.createTenantColumnForTable(tableName)
+            1 * rlsPolicyHelper.enrichBuilderWithTableRLSPolicy(builder, table, tenantTableProperties, posmultenUtilContext)
 
         and: "returned the same object of builder"
             result.is(builder)
