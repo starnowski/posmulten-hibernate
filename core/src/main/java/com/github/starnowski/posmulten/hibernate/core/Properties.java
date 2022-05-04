d52ef9dd75a9eb0f9fe8ca8239e806e800d4101f
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-05-04 17:20:45 +0200

    #6 - Added implementation. [skip ci]

--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/Properties.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/Properties.java
@@ -9,4 +9,5 @@ public class Properties {
     public static final String TENANT_ID_INVALID_VALUES = "hibernate.posmulten.tenant.id.values.blacklist";
     public static final String TENANT_ID_DUMMY_VALUE = "hibernate.posmulten.tenant.id.value.dummy";
     public static final String TENANT_COLUMN_JAVA_TYPE = "hibernate.posmulten.tenant.column.java.type";
+    public static final String TENANT_COLUMN_JAVA_TYPE_CUSTOM_RESOLVER = "hibernate.posmulten.tenant.column.java.type.custom.resolver";
 }
--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/CurrentTenantPreparedStatementSetterInitiator.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/CurrentTenantPreparedStatementSetterInitiator.java
@@ -6,6 +6,7 @@ import org.hibernate.service.spi.ServiceRegistryImplementor;
 import java.util.Map;
 
 import static com.github.starnowski.posmulten.hibernate.core.Properties.TENANT_COLUMN_JAVA_TYPE;
+import static com.github.starnowski.posmulten.hibernate.core.Properties.TENANT_COLUMN_JAVA_TYPE_CUSTOM_RESOLVER;
 
 public class CurrentTenantPreparedStatementSetterInitiator implements StandardServiceInitiator<ICurrentTenantPreparedStatementSetter> {
     @Override
@@ -14,6 +15,16 @@ public class CurrentTenantPreparedStatementSetterInitiator implements StandardSe
             switch ((String) map.get(TENANT_COLUMN_JAVA_TYPE)){
                 case "long":
                     return new CurrentTenantPreparedStatementSetterForLong();
+                case "custom":
+                    if (map.containsKey(TENANT_COLUMN_JAVA_TYPE_CUSTOM_RESOLVER)) {
+                        try {
+                            return (ICurrentTenantPreparedStatementSetter)Class.forName((String)map.get(TENANT_COLUMN_JAVA_TYPE_CUSTOM_RESOLVER)).newInstance();
+                        } catch (Exception e) {
+                            throw new RuntimeException(e);
+                        }
+                    } else {
+                        //TODO return exception about missing property
+                    }
                 case "string":
             }
         }
