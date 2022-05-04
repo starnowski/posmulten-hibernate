8adf5d4ee352d56d6208071a6a2209bb98136b09
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-05-04 17:09:54 +0200

    #6 - Added implementation. [skip ci]

--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/CurrentTenantPreparedStatementSetterInitiator.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/connections/CurrentTenantPreparedStatementSetterInitiator.java
@@ -5,9 +5,18 @@ import org.hibernate.service.spi.ServiceRegistryImplementor;
 
 import java.util.Map;
 
+import static com.github.starnowski.posmulten.hibernate.core.Properties.TENANT_COLUMN_JAVA_TYPE;
+
 public class CurrentTenantPreparedStatementSetterInitiator implements StandardServiceInitiator<ICurrentTenantPreparedStatementSetter> {
     @Override
     public ICurrentTenantPreparedStatementSetter initiateService(Map map, ServiceRegistryImplementor serviceRegistryImplementor) {
+        if (map.containsKey(TENANT_COLUMN_JAVA_TYPE)){
+            switch ((String) map.get(TENANT_COLUMN_JAVA_TYPE)){
+                case "long":
+                    return new CurrentTenantPreparedStatementSetterForLong();
+                case "string":
+            }
+        }
         return new CurrentTenantPreparedStatementSetterForString();
     }
 
