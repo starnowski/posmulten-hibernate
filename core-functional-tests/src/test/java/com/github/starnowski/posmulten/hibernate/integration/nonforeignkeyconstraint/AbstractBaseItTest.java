eeaae043393f1b3247f9d0a8635952d61f00c0cb
starnowski <33316705+starnowski@users.noreply.github.com>
2022-05-10 19:52:18 +0200

    #10 Tests code refactoring. [skip ci]

---
+++ core-functional-tests/src/test/java/com/github/starnowski/posmulten/hibernate/integration/nonforeignkeyconstraint/AbstractBaseItTest.java
@@ -0,0 +1,84 @@
+package com.github.starnowski.posmulten.hibernate.integration.nonforeignkeyconstraint;
+
+import com.github.starnowski.posmulten.hibernate.core.connections.CurrentTenantPreparedStatementSetterInitiator;
+import com.github.starnowski.posmulten.hibernate.core.connections.SharedSchemaConnectionProviderInitiatorAdapter;
+import com.github.starnowski.posmulten.hibernate.core.context.DefaultSharedSchemaContextBuilderMetadataEnricherProviderInitiator;
+import com.github.starnowski.posmulten.hibernate.core.context.DefaultSharedSchemaContextBuilderProviderInitiator;
+import com.github.starnowski.posmulten.hibernate.core.context.metadata.PosmultenUtilContextInitiator;
+import com.github.starnowski.posmulten.hibernate.core.schema.SchemaCreatorStrategyContextInitiator;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+
+public class AbstractBaseItTest {
+
+    private static SessionFactory primarySessionFactory;
+    private static SessionFactory schemaCreatorSessionFactory;
+    protected Session schemaCreatorSession;
+    protected Session primarySession;
+
+    protected SessionFactory getPrimarySessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .addInitiator(new SharedSchemaConnectionProviderInitiatorAdapter())
+                .addInitiator(new DefaultSharedSchemaContextBuilderProviderInitiator())
+                .addInitiator(new CurrentTenantPreparedStatementSetterInitiator())
+                .configure("hibernate.nonforeignkeyconstraint.cfg.xml")
+                .build();
+
+        SessionFactory factory = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory();
+        return factory;
+    }
+
+    protected SessionFactory getSchemaCreatorSessionFactory() {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .addInitiator(new SchemaCreatorStrategyContextInitiator())
+                .addInitiator(new DefaultSharedSchemaContextBuilderProviderInitiator())
+                .addInitiator(new DefaultSharedSchemaContextBuilderMetadataEnricherProviderInitiator())
+                .addInitiator(new PosmultenUtilContextInitiator())
+                .configure("hibernate.nonforeignkeyconstraint.schema-creator.cfg.xml")
+                .build();
+
+        SessionFactory factory = new MetadataSources(registry)
+                .buildMetadata().buildSessionFactory();
+        return factory;
+    }
+
+    @BeforeSuite(groups = "Integration tests")
+    public void prepareDatabase() {
+        schemaCreatorSessionFactory = getSchemaCreatorSessionFactory();
+        primarySessionFactory = getPrimarySessionFactory();
+    }
+
+    @BeforeClass
+    public void openSession() {
+        schemaCreatorSession = schemaCreatorSessionFactory.openSession();
+    }
+
+    protected Session openPrimarySession() {
+        return primarySessionFactory.openSession();
+    }
+
+    @AfterTest
+    public void closeSession() {
+        try {
+            if (schemaCreatorSession != null) {
+                schemaCreatorSession.close();
+            }
+        } catch (Exception ex) {
+            // do nothing
+        }
+        try {
+            if (primarySession != null) {
+                primarySession.close();
+            }
+        } catch (Exception ex) {
+            // do nothing
+        }
+    }
+}
