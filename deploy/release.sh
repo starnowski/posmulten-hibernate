48aa706464dd06cea820644ddeea3072f563f235
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-05-29 22:12:56 +0200

    #12 - Code refactoring [skip ci]

--- deploy/release.sh
+++ deploy/release.sh
@@ -17,10 +17,13 @@ trap removeSecretKey EXIT SIGINT
 # Decrypting key files
 openssl aes-256-cbc -d -pass pass:$ENCRYPTION_PASSWORD -pbkdf2 -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg
 openssl aes-256-cbc -d -pass pass:$ENCRYPTION_PASSWORD -pbkdf2 -in $GPG_DIR/public.gpg.enc -out $GPG_DIR/pubring.gpg
-# Prod
-#"${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
-#Test
+
 gpg --batch --yes --pinentry-mode loopback --import $GPG_DIR/secring.gpg
 gpg --import $GPG_DIR/pubring.gpg
-"${DIRNAME}/../mvnw" clean install -DperformRelease=true -DskipTests=true
+
+#Test
+#"${DIRNAME}/../mvnw" clean install -DperformRelease=true -DskipTests=true
+
+# Prod
+"${DIRNAME}/../mvnw" deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -P maven-central-deploy
 exit $?
