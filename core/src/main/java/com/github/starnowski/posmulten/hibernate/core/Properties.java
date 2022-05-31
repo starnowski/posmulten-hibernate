4456265189209716970d1c0b337d38e745ed65dc
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2022-05-31 08:21:44 +0200

    #8 Added documentation entry [skip ci]

--- core/src/main/java/com/github/starnowski/posmulten/hibernate/core/Properties.java
+++ core/src/main/java/com/github/starnowski/posmulten/hibernate/core/Properties.java
@@ -7,6 +7,7 @@ public class Properties {
     public static final String ID_PROPERTY = "hibernate.posmulten.tenant.id.property";
     public static final String TENANT_ID_SET_CURRENT_AS_DEFAULT_FLAG = "hibernate.posmulten.tenant.id.set.current.as.default";
     public static final String TENANT_ID_INVALID_VALUES = "hibernate.posmulten.tenant.id.values.blacklist";
+    //TODO Use dummy value when it was passed and check if value is one of TENANT_ID_INVALID_VALUES values
     public static final String TENANT_ID_DUMMY_VALUE = "hibernate.posmulten.tenant.id.value.dummy";
     public static final String TENANT_COLUMN_JAVA_TYPE = "hibernate.posmulten.tenant.column.java.type";
     public static final String TENANT_COLUMN_JAVA_TYPE_CUSTOM_RESOLVER = "hibernate.posmulten.tenant.column.java.type.custom.resolver";
