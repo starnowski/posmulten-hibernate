646550acbd068f91f6c22cc3cb300bdb6d52adc0
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-06-30 19:55:51 +0200

    #29 Added hibernate6 module

--- hibernate6/src/main/java/com/github/starnowski/posmulten/hibernate/hibernate6/CurrentTenantIdentifierResolverImpl.java
+++ hibernate6/src/main/java/com/github/starnowski/posmulten/hibernate/hibernate6/CurrentTenantIdentifierResolverImpl.java
@@ -1,11 +1,12 @@
-package com.github.starnowski.posmulten.hibernate.hibernate5;
+package com.github.starnowski.posmulten.hibernate.hibernate6;
+
 
 import org.hibernate.context.spi.CurrentTenantIdentifierResolver;
 
 import static com.github.starnowski.posmulten.hibernate.common.context.CurrentTenantContext.getCurrentTenant;
 
-
 public class CurrentTenantIdentifierResolverImpl implements CurrentTenantIdentifierResolver {
+
     public String resolveCurrentTenantIdentifier() {
         return getCurrentTenant();
     }
