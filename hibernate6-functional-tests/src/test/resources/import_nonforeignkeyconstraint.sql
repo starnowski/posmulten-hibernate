41c004bd45d09e98dca4d97094599b325364eb71
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2023-07-05 00:14:46 +0200

    #29 Fixed all tests

--- hibernate6-functional-tests/src/test/resources/import_nonforeignkeyconstraint.sql
+++ hibernate6-functional-tests/src/test/resources/import_nonforeignkeyconstraint.sql
@@ -4,4 +4,4 @@ GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO "posmhib4
 GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO "posmhib4-user";
 
 -- This is required because hibernate does not creates foreign key constraint for JoinColumnsOrFormulas annotation
-ALTER TABLE posts_nonforeignkeyconstraint ADD CONSTRAINT fk_posts_users_author_manual_added FOREIGN KEY (user_id, tenant_id) REFERENCES user_info_nonforeignkeyconstraint;
+ALTER TABLE posts_nonforeignkeyconstraint ADD CONSTRAINT fk_posts_users_author_manual_added FOREIGN KEY (user_id, tenant_id) REFERENCES user_info_nonforeignkeyconstraint(user_id, tenant);
