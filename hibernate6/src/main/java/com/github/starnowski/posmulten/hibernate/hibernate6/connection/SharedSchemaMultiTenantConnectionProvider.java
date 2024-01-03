01935841edcace6a0034eac1925f4632399a44f9
Szymon Tarnowski <33316705+starnowski@users.noreply.github.com>
2024-01-04 00:09:34 +0100

    #43 - Updated Hibernate version to 6.4.0

--- hibernate6/src/main/java/com/github/starnowski/posmulten/hibernate/hibernate6/connection/SharedSchemaMultiTenantConnectionProvider.java
+++ hibernate6/src/main/java/com/github/starnowski/posmulten/hibernate/hibernate6/connection/SharedSchemaMultiTenantConnectionProvider.java
@@ -17,7 +17,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class SharedSchemaMultiTenantConnectionProvider extends AbstractMultiTenantConnectionProvider implements ServiceRegistryAwareService {
+public class SharedSchemaMultiTenantConnectionProvider extends AbstractMultiTenantConnectionProvider<String> implements ServiceRegistryAwareService {
 
     private ConnectionProvider connectionProvider;
     private ISharedSchemaContext context;
